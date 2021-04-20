996d82ddb86c8d310dee39526689674d8c4c1b82
ep1088 <ep1088@qq.com>
2021-04-20 19:03:04 +0800

    clear

--- tests/mysql-client/slave.php
+++ tests/mysql-client/slave.php
@@ -8,10 +8,6 @@
 include __DIR__."/../../vendor/autoload.php";
 define("HOME", dirname(dirname(__DIR__)));
 
-//初始化一些系统常量
-define("WINDOWS", "windows");
-define("LINUX", "linux");
-
 //定义时间区
 if(!date_default_timezone_get() || !ini_get("date.timezone")) {
     date_default_timezone_set("PRC");
